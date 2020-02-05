eee371fafa7251d82180eb547303911ba4f67e47
Wilson Calil <wilson.calil@outlook.com>
2020-02-05 00:13:58 -0600

    Upload of fixings on js code, implementation of background color changes for UV index, and new README file.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # homework-6
+
+The app was created for those who travel a lot and need to know the forecasts for the next days to get ready for the weather in the city destination.
+
+## Development
+
+The app pulls data from OpenWeatherMap API. This data refers to the current day and 5-day forecast. To render the weather information, an event listener was used. Every time the user inputs the name of the city in the input field, the ajax function pulls information from the API using the URL, the API key, and the name of the city.
+
+Another function adds the name of the city in an array. This array is stored on a visible list of cities that keep a record of all cities previously searched. The for each method works to grab each city from the array, creating an <li>, inserting the name of the city, and storing in local storage. The for-each method also inserts an event listener to it, using the same function (renderForecast) that the input uses, just by adding the name of the city as a parameter.
+
+The function renderForecast creates an HTML element every time there is a new input (by clicking on the list of cities or inputting a new city). After adding elements, it pulls the specific value of the object in the array of the API and places it in the HTML elements created.
+
--- script.js
+++ script.js
@@ -1,241 +1,131 @@
-
-    var cities =[];
-
-function renderForecast() {
-
-    // var cities =[];
-
-    var cityInput = $("#cityInput").val().trim();
-    console.log(cityInput)
-    
-    cities.push(cityInput);
-    console.log(JSON.stringify(cities))
-
-    $("#listOfCities").localStorage.getItem("listOfCities");\
-
-    function renderCityButtons() {
-
-        // Loops through the array of cities
-        for (var i = 0; i < cities.length; i++) {
-        
-        
+let oldCitiesList = JSON.parse(localStorage.getItem("listOfCities"))
+var cities = [];
+if (oldCitiesList) {
+    cities = oldCitiesList
+}
+renderCityButtons()
+function renderCityButtons() {
+    // Loops through the array of cities
+    cities.forEach(function (city) {
         var listOfCities = $("<li>");
         listOfCities.addClass("list-group-item");
-        listOfCities.attr("data-name", cities[i]);
-        listOfCities.text(cities[i]);
+        listOfCities.text(city);
         $("#listOfCities").append(listOfCities);
-        localStorage.setItem("listOfCities", JSON.stringify(cities))
-    
-       
-
-        }
-
-    }
-
+        listOfCities.on("click", function () { renderForecast(city) })
+    })
+}
+function saveCity() {
+    localStorage.setItem("listOfCities", JSON.stringify(cities))
+}
+function renderForecast(cityToRender) {
+    // cities.push(cityToRender);
     $("#listOfCities").empty()
-
     renderCityButtons()
-  
-
-    var queryURLCurrentDay = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial" + "&appid=d04875a8abdb33018b444d3bd910bebb";
-
-
+    var queryURLCurrentDay = "https://api.openweathermap.org/data/2.5/weather?q=" + cityToRender + "&units=imperial" + "&appid=d04875a8abdb33018b444d3bd910bebb";
     $.ajax({
         url: queryURLCurrentDay,
         method: "GET"
     }).then(function (response) {
         console.log(response)
-
         // Clear the city section
         $("#cityCard").empty();
-
         // Append the name of the city
         var cityName = $("<h2>").text(response.name);
         $("#cityCard").append(cityName);
-
-
         // Append the date
         var currentDate = new Date();
         var currentDateMoment = moment(currentDate).format("dddd, MMMM Do YYYY");
         var cityDay = $("<h3>").text(currentDateMoment);
         $("#cityCard").append(cityDay);
         $("#cityCard").append("<br>")
-
-
         // Append the temperature
         var temperature = $("<p>").text("Temperature: " + response.main.temp);
         $("#cityCard").append(temperature);
-
         // Append the humidity
-        var humidity = $("<p>").text("Humidity: " + response.main.humidity);
+        var humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
         $("#cityCard").append(humidity);
-
         // Append the wind speed
         var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed);
         $("#cityCard").append(windSpeed);
-
         //Get Longitude and Latitude for the UV index
         var longitude = response.coord.lon;
         console.log(longitude)
-
         var latitude = response.coord.lat;
         console.log(latitude)
-
         //Append the UV Index
         var queryURLuv = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + latitude + "&lon=" + longitude + "&appid=d04875a8abdb33018b444d3bd910bebb";
         // console.log(queryURLuv)
-
         $.ajax({
             url: queryURLuv,
             method: "GET"
         }).then(function (response2) {
             // console.log(response2)
-
-            var uv = $("<p>").text("UV Index: " + response2.value);
+            var uv = $("<p id='uvIndex'>").text("UV Index: " + response2.value);
             $("#cityCard").append(uv);
-
-
+            // Change UV background color
+        if (response2.value < 5){
+            $("#uvIndex").css("background-color", "green")
+            }
+        else if (response2.value >= 5 && response2.value <= 8){
+            $("#uvIndex").css("background-color", "yellow")
+        }
+        else if (response2.value >= 8){
+            $("#uvIndex").css("background-color", "red")
+        }
         });
-
         // Get icon code to append icon
         var iconCode = response.weather[0].icon;
         // console.log(iconCode)
-
-
         //Append the icon
         var queryURLicon = "https://openweathermap.org/img/wn/" + iconCode + ".png";
         console.log(queryURLicon)
-
         $.ajax({
             url: queryURLicon,
             method: "GET"
         }).then(function (response3) {
             // console.log(response3)
-
             $("#weatherIcon").empty()
-
             var icon = $("<img class='bigIcon'>").attr("src", queryURLicon);
             $("#weatherIcon").append(icon);
-
         });
-
-
         //Append the forecast
-        var queryURLforecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial" + "&appid=d04875a8abdb33018b444d3bd910bebb";;
-        console.log(queryURLforecast)
-
+        var queryURLforecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityToRender + "&units=imperial" + "&appid=d04875a8abdb33018b444d3bd910bebb";;
         $.ajax({
             url: queryURLforecast,
             method: "GET"
         }).then(function (response4) {
-            console.log(response4)
-
+            console.log("response", response4)
             $(".column5").empty()
             $(".column13").empty()
             $(".column21").empty()
             $(".column29").empty()
             $(".column37").empty()
-
-            // Forecast Day 1
-            var forecastDay1 = response4.list[5];
-            var dateDay1 = $("<h5>").text(moment(forecastDay1.dt_txt).format("MMMM Do YYYY"))
-            var div1 = $(".column5").append("<div class='cardBody' id='5'></div>");
-            $("#5").append(dateDay1);
-            $("#5").append("Temp: " + response4.list[5].main.temp + "°F");
-            $("#5").append("<br>");
-            $("#5").append("Humidity: " + response4.list[5].main.humidity + "%");
-
-            // Forecast Day 2
-            var forecastDay2 = response4.list[13];
-            var dateDay2 = $("<h5>").text(moment(forecastDay2.dt_txt).format("MMMM Do YYYY"))
-            $(".column13").append("<div class='cardBody' id='13'></div>");
-            $("#13").append(dateDay2);
-            $("#13").append("Temp: " + response4.list[13].main.temp + "°F");
-            $("#13").append("<br>");
-            $("#13").append("Humidity: " + response4.list[13].main.humidity + "%");
-
-            // Forecast Day 3
-            var forecastDay3 = response4.list[21];
-            var dateDay3 = $("<h5>").text(moment(forecastDay3.dt_txt).format("MMMM Do YYYY"))
-            $(".column21").append("<div class='cardBody' id='21'></div>");
-            $("#21").append(dateDay3);
-            $("#21").append("Temp: " + response4.list[21].main.temp + "°F");
-            $("#21").append("<br>");
-            $("#21").append("Humidity: " + response4.list[21].main.humidity + "%");
-
-            // Forecast Day 4
-            var forecastDay4 = response4.list[29];
-            var dateDay4 = $("<h5>").text(moment(forecastDay4.dt_txt).format("MMMM Do YYYY"))
-            $(".column29").append("<div class='cardBody' id='29'></div>");
-            $("#29").append(dateDay4);
-            $("#29").append("Temp: " + response4.list[29].main.temp + "°F");
-            $("#29").append("<br>");
-            $("#29").append("Humidity: " + response4.list[29].main.humidity + "%");
-
-            // Forecast Day 5
-            var forecastDay5 = response4.list[37];
-            var dateDay5 = $("<h5>").text(moment(forecastDay5.dt_txt).format("MMMM Do YYYY"))
-            $(".column37").append("<div class='cardBody' id='37'></div>");
-            $("#37").append(dateDay5);
-            $("#37").append("Temp: " + response4.list[37].main.temp + "°F");
-            $("#37").append("<br>");
-            $("#37").append("Humidity: " + response4.list[37].main.humidity + "%");
-
-            // Get forecast icon code to append icon
-            var forecastIconCode = response4.list[5].weather[0].icon;
-            console.log(forecastIconCode)
-
-            //Append the iconForecast
-            var queryURLiconForecast = "https://openweathermap.org/img/wn/" + forecastIconCode + ".png";
-            // console.log(queryURLicon)
-
-            $.ajax({
-                url: queryURLiconForecast,
-                method: "GET"
-            }).then(function () {
-
-                // Icon Day 1      
-                var icon = $("<img class='icon'>").attr("src", queryURLiconForecast);
-                $("#5").append(icon);
-
-                // Icon Day 2  
-                var icon = $("<img class='icon'>").attr("src", queryURLiconForecast);
-                $("#13").append(icon);
-
-                // Icon Day 3  
-                var icon = $("<img class='icon'>").attr("src", queryURLiconForecast);
-                $("#21").append(icon);
-
-                // Icon Day 4  
-                var icon = $("<img class='icon'>").attr("src", queryURLiconForecast);
-                $("#29").append(icon)
-
-                // Icon Day 5  
-                var icon = $("<img class='icon'>").attr("src", queryURLiconForecast);
-                $("#37").append(icon)
-
+            const targetTimes = [5, 13, 21, 29, 37]
+            targetTimes.forEach((time) => {
+                var forecastDay1 = response4.list[time];
+                var dateDay1 = $("<h5>").text(moment(forecastDay1.dt_txt).format("MMMM Do YYYY"))
+                var div1 = $(`.column${time}`).append(`<div class='cardBody' id='${time}'></div>`);
+                $(`#${time}`).append(dateDay1);
+                $(`#${time}`).append("Temp: " + response4.list[time].main.temp + "°F");
+                $(`#${time}`).append("<br>");
+                $(`#${time}`).append("Humidity: " + response4.list[time].main.humidity + "%");
+                var forecastIconCode = response4.list[time].weather[0].icon;
+                var queryURLiconForecast = "https://openweathermap.org/img/wn/" + forecastIconCode + ".png";
+                $.ajax({
+                    url: queryURLiconForecast,
+                    method: "GET"
+                }).then(function () {
+                    var icon = $("<img class='icon'>").attr("src", queryURLiconForecast);
+                    $(`#${time}`).append(icon);
+                })
             });
-
-
-
         });
-
     });
-
-
-
-
 }
-
-
-
-
 $("#basic-addon2").on("click", function (event) {
     event.preventDefault();
-  
-    renderForecast();  
-
-
+    let cityInput = $("#cityInput").val().trim()
+    cities.push(cityInput)
+    renderForecast(cityInput);
+    saveCity()
 });
-
-
